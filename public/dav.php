60412494f99d1ffdd4553e0ba822fd11ece378db
Per Olofsson <pelle@pqz.se>
2015-06-21 10:56:37 +0200

    report all errors

--- public/dav.php
+++ public/dav.php
@@ -1,12 +1,13 @@
 <?php
-require '../vendor/autoload.php';
-
 use Sabre\DAV;
 use Sabre\CardDAV;
 
+error_reporting(E_ALL);
 set_error_handler(function ($severity, $message, $file, $line) {
         throw new ErrorException($message, 0, $severity, $file, $line);
     });
+
+require '../vendor/autoload.php';
 
 $server = new DAV\Server(new SukatAddressBook('dc=dsv,dc=su,dc=se'));
 $server->setBaseUri($_SERVER['SCRIPT_NAME']);
